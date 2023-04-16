f64d723a2e92bf44b8aede38b4b94fafd81c82a7
sam <sjglover@udel.edu>
2023-04-16 16:11:22 -0400

    Finished the OpenMP function

--- image.c
+++ image.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "image.h"
 #include <pthread.h>
+#include <omp.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -68,6 +69,22 @@ void* convolute_pthread(void* part){
 
 }
 
+void convolute_openmp(Image* srcImage, Image* destImage, Matrix algorithm){
+    int row, pix, bit, span;
+    span = srcImage->bpp * srcImage->bpp;
+
+    # pragma omp parallel
+    for (row = 0; row < srcImage->height; row++){
+        for (int private_pix = 0; private_pix < srcImage->width; private_pix++){
+            for (int private_bit = 0; private_bit < srcImage->bpp; private_bit++){
+                destImage->data[Index(private_pix, row, srcImage->width, private_bit, srcImage->bpp)] = getPixelValue(srcImage, private_pix, row, private_bit, algorithm)''
+            }
+
+        }
+
+    }
+}
+
 //convolute:  Applies a kernel matrix to an image
 //Parameters: srcImage: The image being convoluted
 //            destImage: A pointer to a  pre-allocated (including space for the pixel array) structure to receive the convoluted image.  It should be the same size as srcImage
@@ -107,7 +124,6 @@ enum KernelTypes GetKernelType(char* type){
 //main:
 //argv is expected to take 2 arguments.  First is the source file name (can be jpg, png, bmp, tga).  Second is the lower case name of the algorithm.
 int main(int argc,char** argv){
-    pthread_t first_thread
     long t1,t2;
     t1=time(NULL);
 
@@ -129,7 +145,7 @@ int main(int argc,char** argv){
     destImage.height=srcImage.height;
     destImage.width=srcImage.width;
     destImage.data=malloc(sizeof(uint8_t)*destImage.width*destImage.bpp*destImage.height);
-    convolute(&srcImage,&destImage,algorithms[type]);
+    convolute_openmp(&srcImage,&destImage,algorithms[type]);
     stbi_write_png("output.png",destImage.width,destImage.height,destImage.bpp,destImage.data,destImage.bpp*destImage.width);
     stbi_image_free(srcImage.data);
     
